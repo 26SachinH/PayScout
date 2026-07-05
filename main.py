5b3db210b431460b68eaecb26ee39e15770da062
26SachinH <sachinhebbalakar26@gmail.com>
2026-07-05 09:43:28 +0530

    check and report endpoints built

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/health")
 def health_deck():
     return{"Status" : "ok", "app" : "payscout"}
+
+@app.get("/check")
+def check_number(phone:str):
+    return{
+        "phone" : phone,
+        "risk_score" : 0.0,
+        "message" : "no reports found" 
+    }
+
+@app.post("/report")
+def report_found(phone:str, fraud_type:str, description:str):
+    return{
+        "message" : "report received",
+        "phone" : phone,
+        "fraud_type" : fraud_type
+    }
